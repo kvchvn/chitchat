ac4303c34afe5449e037c8db552ed4d572f2badb
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-03 23:59:38 +0300

    feat(styles): import fonts from next/font/google

---
+++ app/src/styles/font.ts
@@ -0,0 +1,16 @@
+import { Inter, JetBrains_Mono, Poppins } from 'next/font/google';
+
+export const inter = Inter({
+  weight: ['200', '400', '500', '600', '700'],
+  variable: '--font-inter',
+});
+
+export const jetBransMono = JetBrains_Mono({
+  weight: ['300', '400', '600'],
+  variable: '--font-jbmono',
+});
+
+export const poppins = Poppins({
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+});
