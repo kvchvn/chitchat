b4dced5512714eb19a30c7baadb5b04719033aae
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-07 23:10:47 +0300

    chore(auth.ts): custom pages

--- app/src/server/auth.ts
+++ app/src/server/auth.ts
@@ -1,23 +1,14 @@
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
-} from "next-auth";
-import { type Adapter } from "next-auth/adapters";
-import FacebookProvider from "next-auth/providers/facebook";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import YandexProvider from "next-auth/providers/yandex";
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth';
+import { type Adapter } from 'next-auth/adapters';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import YandexProvider from 'next-auth/providers/yandex';
 
-import { env } from "~/env";
-import { db } from "~/server/db";
-import {
-  accounts,
-  sessions,
-  users,
-  verificationTokens,
-} from "~/server/db/schema/auth";
+import { env } from '~/env';
+import { db } from '~/server/db';
+import { accounts, sessions, users, verificationTokens } from '~/server/db/schema/auth';
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -25,13 +16,13 @@ import {
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session extends DefaultSession {
     user: {
       id: string;
       // ...other properties
       // role: UserRole;
-    } & DefaultSession["user"];
+    } & DefaultSession['user'];
   }
 
   // interface User {
@@ -54,6 +45,11 @@ export const authOptions: NextAuthOptions = {
         id: user.id,
       },
     }),
+  },
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in/error',
+    newUser: '/sign-in/welcome',
   },
   adapter: DrizzleAdapter(db, {
     usersTable: users,
