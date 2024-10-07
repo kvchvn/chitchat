d3af5f946ecc9fe8205f2636c5cb10876cce3e1c
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-07 23:05:44 +0300

    feat(tailwind.config): add new colors

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,9 +1,10 @@
 import { type Config } from 'tailwindcss';
+import tailwindcssAnimate from 'tailwindcss-animate';
 import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./src/**/*.tsx'],
-  darkMode: 'selector',
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
@@ -19,14 +20,38 @@ export default {
         primary: {
           light: colors.sky[100],
           dark: colors.sky[900],
+          hover: {
+            light: colors.sky[200],
+            dark: colors.sky[800],
+          },
+          active: {
+            light: colors.sky[300],
+            dark: colors.sky[700],
+          },
         },
         secondary: {
           light: colors.teal[100],
           dark: colors.teal[900],
+          hover: {
+            light: colors.teal[200],
+            dark: colors.teal[800],
+          },
+          active: {
+            light: colors.teal[300],
+            dark: colors.teal[700],
+          },
         },
         accent: {
           light: colors.indigo[100],
           dark: colors.indigo[900],
+          hover: {
+            light: colors.indigo[200],
+            dark: colors.indigo[800],
+          },
+          active: {
+            light: colors.indigo[300],
+            dark: colors.indigo[700],
+          },
         },
         muted: {
           light: colors.gray[100],
@@ -35,14 +60,42 @@ export default {
         highlight: {
           light: colors.amber[100],
           dark: colors.amber[900],
+          hover: {
+            light: colors.amber[200],
+            dark: colors.amber[800],
+          },
+          active: {
+            light: colors.amber[300],
+            dark: colors.amber[700],
+          },
         },
         success: {
           light: colors.emerald[100],
           dark: colors.emerald[900],
+          hover: {
+            light: colors.emerald[200],
+            dark: colors.emerald[800],
+          },
+          active: {
+            light: colors.emerald[300],
+            dark: colors.emerald[700],
+          },
         },
         error: {
           light: colors.rose[100],
           dark: colors.rose[900],
+          hover: {
+            light: colors.rose[200],
+            dark: colors.rose[800],
+          },
+          active: {
+            light: colors.rose[300],
+            dark: colors.rose[700],
+          },
+        },
+        gray: {
+          light: colors.gray[200],
+          dark: colors.gray[700],
         },
         text: {
           light: colors.gray[900],
@@ -55,5 +108,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
