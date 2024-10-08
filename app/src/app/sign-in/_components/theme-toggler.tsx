5485cfadda025b4a85adb7f9b93377d127984d1d
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-08 23:06:08 +0300

    feat(theme-toggler): implement component for sign-in page

---
+++ app/src/app/sign-in/_components/theme-toggler.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { cva } from 'class-variance-authority';
+import { useTheme } from 'next-themes';
+import { Button } from '~/components/ui/button';
+import { Icon } from '~/components/ui/icon';
+import { cn } from '~/lib/utils';
+
+const iconVariants = cva('absolute transition-transform duration-500 scale-30', {
+  variants: {
+    type: {
+      sun: 'translate-x-[200%]',
+      moon: '-translate-x-[200%]',
+    },
+    isOn: {
+      true: 'translate-x-0 scale-100',
+    },
+  },
+});
+
+export const ThemeToggler = () => {
+  const { theme, setTheme } = useTheme();
+
+  const handleClick = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <Button size="icon" onClick={handleClick} className="absolute bottom-4 right-4 overflow-hidden">
+      <Icon
+        scope="global"
+        id="sun"
+        className={cn(iconVariants({ type: 'sun', isOn: theme === 'light' }))}
+      />
+      <Icon
+        scope="global"
+        id="moon"
+        className={cn(iconVariants({ type: 'moon', isOn: theme === 'dark' }))}
+      />
+    </Button>
+  );
+};
