f7ea8f59ef799cd94ae2c55e58dcb9c693d7f223
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-08 22:51:11 +0300

    chore(tailwind): extend theme

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,6 +1,7 @@
 import { type Config } from 'tailwindcss';
 import tailwindcssAnimate from 'tailwindcss-animate';
 import colors from 'tailwindcss/colors';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./src/**/*.tsx'],
@@ -12,7 +13,57 @@ export default {
         mono: ['var(--font-jbmono)'],
         title: ['var(--font-poppins)'],
       },
+      screens: {
+        xs: '480px',
+        ...defaultTheme.screens,
+      },
       colors: {
+        auth: {
+          google: {
+            DEFAULT: 'transparent',
+            hover: {
+              light: colors.slate['100'],
+              dark: colors.slate['700'],
+            },
+            active: {
+              light: colors.slate['200'],
+              dark: colors.slate['600'],
+            },
+          },
+          github: {
+            DEFAULT: 'hsl(215 15.4% 15.3%)',
+            hover: {
+              light: 'hsl(215 15.4% 80%)',
+              dark: 'hsl(215 20% 55%)',
+            },
+            active: {
+              light: 'hsl(215 15.4% 70%)',
+              dark: 'hsl(215 20% 50%)',
+            },
+          },
+          yandex: {
+            DEFAULT: 'hsl(9 97.4% 55.1%)',
+            hover: {
+              light: 'hsl(9 97.4% 75%)',
+              dark: 'hsl(9 50% 40%)',
+            },
+            active: {
+              light: 'hsl(9 85% 65%)',
+              dark: 'hsl(9 50% 50%)',
+            },
+          },
+          facebook: {
+            DEFAULT: 'hsl(217 100% 51.6%)',
+            hover: {
+              light: 'hsl(217 100% 80%)',
+              dark: 'hsl(217 50% 40%)',
+            },
+            active: {
+              light: 'hsl(217 100% 65%)',
+              dark: 'hsl(217 50% 50%)',
+            },
+          },
+        },
         background: {
           light: colors.slate[50],
           dark: colors.slate[900],
@@ -58,14 +109,14 @@ export default {
           dark: colors.gray[800],
         },
         highlight: {
-          light: colors.amber[100],
+          light: colors.amber[500],
           dark: colors.amber[900],
           hover: {
-            light: colors.amber[200],
+            light: colors.amber[400],
             dark: colors.amber[800],
           },
           active: {
-            light: colors.amber[300],
+            light: colors.amber[600],
             dark: colors.amber[700],
           },
         },
@@ -99,12 +150,15 @@ export default {
         },
         text: {
           light: colors.gray[900],
-          dark: colors.gray[100],
+          dark: colors.gray[200],
         },
         'text-muted': {
           light: colors.gray[500],
           dark: colors.gray[400],
         },
+      },
+      scale: {
+        '30': '0.3',
       },
     },
   },
