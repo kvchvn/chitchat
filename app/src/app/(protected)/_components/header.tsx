3eeed03e24439f47d6c833eba6c3d8691b317347
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-11 18:06:52 +0300

    feat(header): add base layout for protected routes header

---
+++ app/src/app/(protected)/_components/header.tsx
@@ -0,0 +1,32 @@
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+import { ThemeToggler } from '~/components/ui/theme-toggler';
+import { getNameInitials } from '~/lib/utils';
+import { getServerAuthSession } from '~/server/auth';
+
+export const Header = async () => {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    return null;
+  }
+
+  const nameInitials = getNameInitials(session.user.name);
+
+  return (
+    <header className="flex items-center gap-2 py-2">
+      <div className="ml-auto flex items-center gap-2">
+        <ThemeToggler className="h-8 w-8" />
+        <div className="flex items-center gap-1">
+          <Avatar className="rounded-md">
+            <AvatarImage
+              src={session.user.image ?? undefined}
+              alt={session.user.name ?? "user's avatar"}
+            />
+            <AvatarFallback>{nameInitials}</AvatarFallback>
+          </Avatar>
+          <span className="text-sm">{session.user.name ?? 'nice person'}</span>
+        </div>
+      </div>
+    </header>
+  );
+};
