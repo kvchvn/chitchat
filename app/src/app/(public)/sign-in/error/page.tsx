f1ed560ffc1c4a341db94d32e877e897c8081d86
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-16 23:38:50 +0300

    feat(/sign-in/error): add redirect if searchParams are not

--- app/src/app/(public)/sign-in/error/page.tsx
+++ app/src/app/(public)/sign-in/error/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import { Button } from '~/components/ui/button';
 import { ROUTES } from '~/constants/routes';
 
@@ -7,16 +8,20 @@ export default function SignInErrorPage({
 }: {
   searchParams: Record<string, string>;
 }) {
-  return (
-    <>
-      <h2>{'error' in searchParams ? searchParams.error : ''}</h2>
-      <p className="text-sm">
-        Your sign in request is failed. Please, try to sign in via another provider or go back
-        later, maybe there are temporary troubles.
-      </p>
-      <Button asChild>
-        <Link href={ROUTES.signIn}>Try again</Link>
-      </Button>
-    </>
-  );
+  if (!('error' in searchParams)) {
+    redirect(ROUTES.signIn);
+  } else {
+    return (
+      <>
+        <h2>{searchParams.error}</h2>
+        <p className="text-sm">
+          Your sign in request is failed. Please, try to sign in via another provider or go back
+          later, maybe there are temporary troubles.
+        </p>
+        <Button asChild>
+          <Link href={ROUTES.signIn}>Try again</Link>
+        </Button>
+      </>
+    );
+  }
 }
