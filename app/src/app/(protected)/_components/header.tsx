7b15dcf122556022b677d32509a7622e0d1ab18d
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-19 16:29:15 +0300

    refactor(protected-header): update styles

--- app/src/app/(protected)/_components/header.tsx
+++ app/src/app/(protected)/_components/header.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
 import { ThemeToggler } from '~/components/ui/theme-toggler';
+import { Wrapper } from '~/components/ui/wrapper';
 import { getNameInitials } from '~/lib/utils';
 import { getServerAuthSession } from '~/server/auth';
 
@@ -13,10 +14,10 @@ export const Header = async () => {
   const nameInitials = getNameInitials(session.user.name);
 
   return (
-    <header className="flex items-center gap-2 py-2">
-      <div className="ml-auto flex items-center gap-2">
-        <ThemeToggler className="h-8 w-8 rounded-full" />
-        <div className="flex items-center gap-1">
+    <header className="bg-slate-100 py-2 dark:bg-slate-700">
+      <Wrapper className="flex items-center">
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggler className="h-8 w-8 rounded-full" />
           <Avatar>
             <AvatarImage
               src={session.user.image ?? undefined}
@@ -24,9 +25,8 @@ export const Header = async () => {
             />
             <AvatarFallback className="text-sm">{nameInitials}</AvatarFallback>
           </Avatar>
-          {/* <span className="text-sm">{session.user.name ?? 'nice person'}</span> */}
         </div>
-      </div>
+      </Wrapper>
     </header>
   );
 };
