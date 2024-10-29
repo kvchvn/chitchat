6b8b067174a2114cd466894c80ce467fc1dbbfd4
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-30 00:09:54 +0300

    feat(requests-list): improve components

--- app/src/app/(protected)/friends/@requests/_components/outcoming-requests-list.tsx
+++ app/src/app/(protected)/friends/@requests/_components/outcoming-requests-list.tsx
@@ -1,6 +1,9 @@
 import { type User } from 'next-auth';
 import { UserItem } from '~/app/(protected)/_components/user-item';
+import { UsersContainer } from '~/app/(protected)/_components/users-container';
 import { UsersList } from '~/app/(protected)/_components/users-list';
+import { UsersPagination } from '~/app/(protected)/_components/users-pagination';
+import { getUsersSlice } from '~/lib/utils';
 import { OutcomingRequestAction } from './outcoming-request-action';
 
 const outcomingRequests: Pick<User, 'id' | 'name' | 'image'>[] = [
@@ -11,19 +14,33 @@ const outcomingRequests: Pick<User, 'id' | 'name' | 'image'>[] = [
   },
 ];
 
+const REQUESTS_LIMIT = 5;
+// temporary
+const PAGE = 1;
+
 export const OutcomingRequestsList = () => {
+  const moreThanLimit = outcomingRequests.length > REQUESTS_LIMIT;
+  const outcomingRequestsSlice = getUsersSlice({
+    users: outcomingRequests,
+    page: PAGE,
+    perPage: REQUESTS_LIMIT - 1,
+  });
+
   return (
-    <UsersList>
-      {outcomingRequests.map((outcomingRequest) => (
-        <UserItem
-          key={outcomingRequest.id}
-          id={outcomingRequest.id}
-          name={outcomingRequest.name}
-          image={outcomingRequest.image}
-        >
-          <OutcomingRequestAction id={outcomingRequest.id} />
-        </UserItem>
-      ))}
-    </UsersList>
+    <UsersContainer>
+      <UsersList>
+        {outcomingRequestsSlice.map((outcomingRequest) => (
+          <UserItem
+            key={outcomingRequest.id}
+            id={outcomingRequest.id}
+            name={outcomingRequest.name}
+            image={outcomingRequest.image}
+          >
+            <OutcomingRequestAction id={outcomingRequest.id} />
+          </UserItem>
+        ))}
+        {moreThanLimit && <UsersPagination />}
+      </UsersList>
+    </UsersContainer>
   );
 };
