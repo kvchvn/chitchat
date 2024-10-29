5c4a39d8d425977406c6e020ba105b3f1c0c2779
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-30 00:03:46 +0300

    chore(utils): create getUsersSlice

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,10 +1,12 @@
 import { clsx, type ClassValue } from 'clsx';
+import { type User } from 'next-auth';
 import { twMerge } from 'tailwind-merge';
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
+// for user's avatar
 export const getNameInitials = (name: string | undefined | null) => {
   if (!name) {
     return 'U';
@@ -17,4 +19,17 @@ export const getNameInitials = (name: string | undefined | null) => {
 
   // U means user
   return !firstLetter && !secondLetter ? 'U' : `${firstLetter}${secondLetter}`;
+};
+
+// for showing slice of friends/requests on the page
+export const getUsersSlice = ({
+  users,
+  page,
+  perPage,
+}: {
+  users: Pick<User, 'id' | 'name' | 'image'>[];
+  page: number;
+  perPage: number;
+}) => {
+  return users.length > perPage + 1 ? users.slice(perPage * (page - 1), perPage * page) : users;
 };
