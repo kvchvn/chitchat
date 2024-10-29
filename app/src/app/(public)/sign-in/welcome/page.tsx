cc4a6d51ccca4c06bec709862d4f4f75e96b0fc3
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-30 00:07:29 +0300

    fix(welcome-page): fix condition

--- app/src/app/(public)/sign-in/welcome/page.tsx
+++ app/src/app/(public)/sign-in/welcome/page.tsx
@@ -10,7 +10,9 @@ export default async function SignInWelcomePage() {
   const session = await getServerAuthSession();
 
   if (session) {
-    await api.user.makeAsNotNew({ id: session.user.id });
+    if (session.user.isNewUser) {
+      await api.user.makeAsNotNew({ id: session.user.id });
+    }
   } else {
     redirect(ROUTES.signIn);
   }
