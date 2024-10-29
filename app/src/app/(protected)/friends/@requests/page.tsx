a2138fac05ea671902f8677610d47f727ecee1f6
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-30 00:11:14 +0300

    fix(requests-page): fix tabs style

--- app/src/app/(protected)/friends/@requests/page.tsx
+++ app/src/app/(protected)/friends/@requests/page.tsx
@@ -15,12 +15,15 @@ export default function FriendRequests() {
       <Link href={ROUTES.allFriends} className="link-ghost">
         <h2 className="font-title">Friend Requests</h2>
       </Link>
-      <Tabs defaultValue={TabsValue.Incoming} className="w-full">
-        <TabsList className="mb-4">
+      <Tabs
+        defaultValue={TabsValue.Incoming}
+        className="flex h-full w-full flex-col items-stretch gap-4"
+      >
+        <TabsList>
           <TabsTrigger value={TabsValue.Incoming}>Incoming</TabsTrigger>
           <TabsTrigger value={TabsValue.Outcoming}>Outcoming</TabsTrigger>
         </TabsList>
-        <TabsContent value={TabsValue.Incoming}>
+        <TabsContent value={TabsValue.Incoming} className="h-full">
           <IncomingRequestsList />
         </TabsContent>
         <TabsContent value={TabsValue.Outcoming}>
