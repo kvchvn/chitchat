e2abcc235e05c20c55ab50f591cf57aa42a15d55
Anton Kachan <aa.kvchvn@gmail.com>
2024-10-30 23:26:15 +0300

    feat(outcoming-requests): show no-users-container if users are not

--- app/src/app/(protected)/friends/@requests/_components/outcoming-requests-list.tsx
+++ app/src/app/(protected)/friends/@requests/_components/outcoming-requests-list.tsx
@@ -1,4 +1,5 @@
 import { type User } from 'next-auth';
+import { NoUsersContainer } from '~/app/(protected)/_components/no-users-container';
 import { UserItem } from '~/app/(protected)/_components/user-item';
 import { UsersContainer } from '~/app/(protected)/_components/users-container';
 import { UsersList } from '~/app/(protected)/_components/users-list';
@@ -7,11 +8,11 @@ import { getUsersSlice } from '~/lib/utils';
 import { OutcomingRequestAction } from './outcoming-request-action';
 
 const outcomingRequests: Pick<User, 'id' | 'name' | 'image'>[] = [
-  {
-    id: '1',
-    name: 'Good Man',
-    image: undefined,
-  },
+  // {
+  //   id: '1',
+  //   name: 'Good Man',
+  //   image: undefined,
+  // },
 ];
 
 const REQUESTS_LIMIT = 5;
@@ -19,6 +20,15 @@ const REQUESTS_LIMIT = 5;
 const PAGE = 1;
 
 export const OutcomingRequestsList = () => {
+  if (!outcomingRequests.length) {
+    return (
+      <NoUsersContainer>
+        <p>You don&apos;t have outcoming friend requests.</p>
+        <p>Propose someone to be your friend and have your common chat.</p>
+      </NoUsersContainer>
+    );
+  }
+
   const moreThanLimit = outcomingRequests.length > REQUESTS_LIMIT;
   const outcomingRequestsSlice = getUsersSlice({
     users: outcomingRequests,
