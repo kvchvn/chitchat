03910f247e64308d027e8441ad4f3877895edb17
Anton Kachan <aa.kvchvn@gmail.com>
2024-11-05 23:55:43 +0300

    feat(routers): add user related actions

--- app/src/server/api/routers/user.ts
+++ app/src/server/api/routers/user.ts
@@ -1,7 +1,8 @@
-import { and, eq, lt } from 'drizzle-orm';
+import { and, eq, lt, ne } from 'drizzle-orm';
 import { z } from 'zod';
 import { sessions, users } from '~/server/db/schema/auth';
-import { createTRPCRouter, publicProcedure } from '../trpc';
+import { friendRequests } from '~/server/db/schema/friends';
+import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc';
 
 export const userRouter = createTRPCRouter({
   // getById: publicProcedure.input(z.object({ id: z.string() })).query(async ({ ctx, input }) => {
@@ -31,5 +32,76 @@ export const userRouter = createTRPCRouter({
         .returning();
 
       return expiredSessions;
+    }),
+  getAllUsersExceptCurrent: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const allUsers = await ctx.db.query.users.findMany({ where: ne(users.id, input.id) });
+
+      return allUsers;
+    }),
+  getUserFriends: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const user = await ctx.db.query.users.findFirst({
+        where: eq(users.id, input.id),
+        with: {
+          friends1: true,
+        },
+      });
+
+      const userFriendsPromises = user?.friends1.map(async (friendship) => {
+        const friendId = input.id === friendship.user1Id ? friendship.user2Id : friendship.user1Id;
+        return await ctx.db.query.users.findFirst({
+          where: eq(users.id, friendId),
+          columns: {
+            id: true,
+            name: true,
+            image: true,
+          },
+        });
+      });
+
+      if (userFriendsPromises) {
+        return Promise.allSettled(userFriendsPromises);
+      } else {
+        return null;
+      }
+    }),
+  getUserIncomingFriendRequests: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const requests = await ctx.db
+        .select({
+          sender: {
+            id: users.id,
+            name: users.name,
+            image: users.image,
+          },
+          status: friendRequests.status,
+        })
+        .from(friendRequests)
+        .where(eq(friendRequests.receiverId, input.id))
+        .leftJoin(users, eq(users.id, friendRequests.senderId));
+
+      return requests;
+    }),
+  getUserOutcomingFriendRequests: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const requests = await ctx.db
+        .select({
+          receiver: {
+            id: users.id,
+            name: users.name,
+            image: users.image,
+          },
+          status: friendRequests.status,
+        })
+        .from(friendRequests)
+        .where(eq(friendRequests.senderId, input.id))
+        .leftJoin(users, eq(users.id, friendRequests.receiverId));
+
+      return requests;
     }),
 });
--- app/src/server/db/index.ts
+++ app/src/server/db/index.ts
@@ -3,7 +3,7 @@ import postgres from 'postgres';
 
 import { env } from '~/env';
 import * as authSchema from './schema/auth';
-import * as exampleSchema from './schema/message';
+import * as friendsSchema from './schema/friends';
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
@@ -16,6 +16,6 @@ const globalForDb = globalThis as unknown as {
 const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
 if (env.NODE_ENV !== 'production') globalForDb.conn = conn;
 
-const schema = { ...authSchema, ...exampleSchema };
+const schema = { ...authSchema, ...friendsSchema };
 
 export const db = drizzle(conn, { schema });
