4af985d9b25b5e5a59caf46997e13e7bb9257e3b
Anton Kachan <aa.kvchvn@gmail.com>
2024-11-16 20:37:28 +0300

    chore(next.config): set remotePatterns for avatars' urls

--- app/next.config.js
+++ app/next.config.js
@@ -6,8 +6,29 @@ await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
+  reactStrictMode: true,
   experimental: {
     serverComponentsExternalPackages: ['pino', 'pino-pretty'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.yandex.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+      },
+    ],
   },
 };
 
