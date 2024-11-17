de611f28ebe87a3a73bb44bcd2dcf511681d39e1
Anton Kachan <aa.kvchvn@gmail.com>
2024-11-17 10:32:58 +0300

    refactor(routers): remove unused messageRouter

--- app/src/server/api/root.ts
+++ app/src/server/api/root.ts
@@ -1,6 +1,5 @@
+import { userRouter } from '~/server/api/routers/user';
 import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
-import { messageRoute } from './routers/message';
-import { userRouter } from './routers/user';
 
 /**
  * This is the primary router for your server.
@@ -8,7 +7,6 @@ import { userRouter } from './routers/user';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  message: messageRoute,
   user: userRouter,
 });
 
--- app/src/server/db/schema/message.ts
+++
@@ -1,7 +0,0 @@
-import { serial, varchar } from 'drizzle-orm/pg-core';
-import { createTable } from '../table-creator';
-
-export const messages = createTable('message', {
-  id: serial('id').primaryKey(),
-  text: varchar('text', { length: 256 }).notNull(),
-});
