befafad7e376c1fd9d134f98354000238a7640c0
Anton Kachan <aa.kvchvn@gmail.com>
2024-11-28 00:27:06 +0300

    feat(suggestion-action): implement component

--- app/src/app/(protected)/friends/@suggestions/_components/suggestion-action.tsx
+++ app/src/app/(protected)/friends/@suggestions/_components/suggestion-action.tsx
@@ -1,15 +1,60 @@
-import { Button } from '~/components/ui/button';
-import { Icon } from '~/components/ui/icon';
+'use client';
+
+import { AcceptFriendRequestAction } from '~/app/(protected)/_components/actions/accept-friend-request-action';
+import { CancelFriendRequestAction } from '~/app/(protected)/_components/actions/cancel-friend-request-action';
+import { RejectFriendRequestAction } from '~/app/(protected)/_components/actions/reject-friend-request-action';
+import { SendFriendRequestAction } from '~/app/(protected)/_components/actions/send-friend-request-action';
 
 type Props = {
-  id: string;
+  suggestedId: string;
+  userId: string;
+  suggestedName: string | null;
+  hasSentRequest: boolean;
+  hasReceivedRequest: boolean;
 };
 
-export const SuggestionAction = ({ id }: Props) => {
-  return (
-    <Button variant="ghost" className="ml-auto">
-      <Icon scope="friends" id="add-friend" />
-      Add to friends
-    </Button>
-  );
+export const SuggestionAction = ({
+  suggestedId,
+  userId,
+  hasSentRequest,
+  hasReceivedRequest,
+  suggestedName,
+}: Props) => {
+  switch (true) {
+    case !hasReceivedRequest && !hasSentRequest:
+      return (
+        <SendFriendRequestAction
+          receiverId={suggestedId}
+          senderId={userId}
+          receiverName={suggestedName}
+          invalidateKey="getSuggestedUsers"
+        />
+      );
+    case hasSentRequest:
+      return (
+        <CancelFriendRequestAction
+          receiverId={suggestedId}
+          senderId={userId}
+          receiverName={suggestedName}
+          invalidateKey="getSuggestedUsers"
+        />
+      );
+    case hasReceivedRequest:
+      return (
+        <>
+          <AcceptFriendRequestAction
+            receiverId={userId}
+            senderId={suggestedId}
+            senderName={suggestedName}
+            invalidateKey="getSuggestedUsers"
+          />
+          <RejectFriendRequestAction
+            receiverId={userId}
+            senderId={suggestedId}
+            senderName={suggestedName}
+            invalidateKey="getSuggestedUsers"
+          />
+        </>
+      );
+  }
 };
