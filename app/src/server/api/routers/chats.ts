786ebffc5a3bede5be946fad42c74c1326748571
Anton Kachan <aa.kvchvn@gmail.com>
2025-02-09 23:22:37 +0300

    feat(chat-router): add procedure "create"

--- app/src/server/api/routers/chats.ts
+++ app/src/server/api/routers/chats.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from '@trpc/server';
 import { and, eq, or } from 'drizzle-orm';
 import { z } from 'zod';
 import { chats } from '~/server/db/schema/chats';
@@ -7,7 +8,7 @@ export const chatsRouter = createTRPCRouter({
   getByMembersIds: protectedProcedure
     .input(z.object({ userId: z.string(), companionId: z.string() }))
     .query(async ({ ctx, input }) => {
-      const chat = await ctx.db
+      const [chat] = await ctx.db
         .select()
         .from(chats)
         .where(
@@ -17,6 +18,33 @@ export const chatsRouter = createTRPCRouter({
           )
         );
 
-      return chat[0] ?? null;
+      if (!chat) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Chat was not found',
+        });
+      }
+
+      return chat;
+    }),
+  create: protectedProcedure
+    .input(z.object({ userId: z.string(), companionId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      if (!input.userId || !input.companionId) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Empty id/ids were passed',
+        });
+      }
+
+      const [newChat] = await ctx.db
+        .insert(chats)
+        .values({
+          userId1: input.userId,
+          userId2: input.companionId,
+        })
+        .returning();
+
+      return newChat;
     }),
 });
