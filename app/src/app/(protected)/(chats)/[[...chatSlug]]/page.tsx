a47946846f2c9bf59ffb4ddeb73382efefaf5179
Anton Kachan <aa.kvchvn@gmail.com>
2025-02-09 23:32:49 +0300

    refactor(chat): update render

--- app/src/app/(protected)/(chats)/[[...chatSlug]]/page.tsx
+++ app/src/app/(protected)/(chats)/[[...chatSlug]]/page.tsx
@@ -1,8 +1,8 @@
-import { getServerAuthSession } from '~/server/auth';
-import { api } from '~/trpc/server';
 import { redirect } from 'next/navigation';
 import { ChatNotFound } from '~/components/chat/chat-not-found';
-import { ChatIsNotCreated } from '~/components/chat/chat-is-not-created';
+import { ChatWindow } from '~/components/chat/chat-window';
+import { getServerAuthSession } from '~/server/auth';
+import { api } from '~/trpc/server';
 
 export default async function ChatPage(props: { params: Promise<{ chatSlug: string[] }> }) {
   const [params, session] = await Promise.all([props.params, getServerAuthSession()]);
@@ -11,24 +11,16 @@ export default async function ChatPage(props: { params: Promise<{ chatSlug: stri
     redirect(`/${params.chatSlug[0]}`);
   }
 
-  const companionId = params?.chatSlug?.[0] ?? '';
-
-  const existingCompanionPromise = api.users.isExisting({ id: companionId });
-  const chatPromise = api.chats.getByMembersIds({ userId: session!.user.id, companionId });
-
-  const [companion, chat] = await Promise.all([existingCompanionPromise, chatPromise]);
+  const companion = await api.users.isExisting({ id: params?.chatSlug?.[0] });
 
   if (!companion) {
     return <ChatNotFound />;
   }
-
-  if (!chat) {
-    return <ChatIsNotCreated />;
-  }
-
   return (
-    <section className="ml-2 h-full border">
-      <header>headera</header>
-    </section>
+    <ChatWindow
+      userId={session!.user.id}
+      companionId={companion.id}
+      companionName={companion.name}
+    />
   );
 }
