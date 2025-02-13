aefe00a5ea1f6dae5a84328ee226fb1d26ec70cd
Anton Kachan <aa.kvchvn@gmail.com>
2025-02-13 09:00:52 +0300

    chore(messages-router): implement base routes

---
+++ app/src/server/api/routers/messages.ts
@@ -0,0 +1,39 @@
+import { TRPCError } from '@trpc/server';
+import { eq } from 'drizzle-orm';
+import { z } from 'zod';
+import { messages } from '~/server/db/schema/messages';
+import { createTRPCRouter, protectedProcedure } from '../trpc';
+
+export const messagesRouter = createTRPCRouter({
+  getByChatId: protectedProcedure
+    .input(z.object({ chatId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.select().from(messages).where(eq(messages.chatId, input.chatId));
+    }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        chatId: z.string(),
+        text: z.string(),
+        senderId: z.string(),
+        receiverId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (!input.chatId || !input.senderId || !input.receiverId) {
+        throw new TRPCError({ code: 'BAD_REQUEST', message: 'Invalid data were passed' });
+      }
+
+      const [newMessage] = await ctx.db
+        .insert(messages)
+        .values({
+          chatId: input.chatId,
+          text: input.text,
+          senderId: input.senderId,
+          receiverId: input.receiverId,
+        })
+        .returning();
+
+      return newMessage;
+    }),
+});
