86da255b524aa1c1495d4182e5c0470d7a154e7e
Anton Kachan <aa.kvchvn@gmail.com>
2025-02-15 19:05:01 +0300

    chore(message): update styles

--- app/src/components/chat/message.tsx
+++ app/src/components/chat/message.tsx
@@ -9,8 +9,10 @@ export const Message = ({ text, fromCurrentUser }: Props) => {
   return (
     <li
       className={cn(
-        fromCurrentUser && 'self-message self-end bg-zinc-200',
-        'w-fit max-w-[80%] break-words rounded-3xl border border-zinc-300 px-6 py-1 leading-6'
+        'w-fit max-w-[80%] break-words rounded-3xl border px-6 py-1 leading-6',
+        fromCurrentUser &&
+          'self-message self-end border-primary-hover-light bg-primary-light dark:border-primary-hover-dark dark:bg-primary-dark',
+        !fromCurrentUser && 'companion-message border-zinc-400'
       )}>
       {text}
     </li>
