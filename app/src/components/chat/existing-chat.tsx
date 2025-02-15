b0a0338f03f8ec3cd7a2275913e98eb8e0dc2fe8
Anton Kachan <aa.kvchvn@gmail.com>
2025-02-15 19:05:37 +0300

    feat(messages-list): implement component with subscription to new messages

--- app/src/components/chat/existing-chat.tsx
+++ app/src/components/chat/existing-chat.tsx
@@ -1,59 +1,26 @@
 import { useEffect, useRef } from 'react';
-import { Message } from '~/components/chat/message';
+import { ChatForm } from '~/components/chat/chat-form';
+import { MessagesList } from '~/components/chat/messages-list';
 import { type ChatPretty } from '~/server/db/schema/chats';
 import { type ChatMessage } from '~/server/db/schema/messages';
-import { ChatForm } from './chat-form';
 
 type Props = {
   chat: ChatPretty;
   messages: (ChatMessage | null)[];
 };
 
-// TODO: remove
-const messages = [
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 222 Message 222 Message 222', userId: '2' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 1 Message 1', userId: '1' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 222', userId: '2' },
-  { text: 'Message 1', userId: '1' },
-  { text: 'Message 222', userId: '2' },
-];
-
 export const ExistingChat = ({ chat, messages }: Props) => {
   const bottomOfListRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
     bottomOfListRef.current?.scrollIntoView();
-  }, []);
+  }, [messages.length]);
 
   return (
     <>
       {messages.length ? (
         <div className="mb-4 mt-2 w-[calc(100%+8px)] grow overflow-y-auto pr-[8px] scrollbar scrollbar-track-rounded-lg scrollbar-thumb-rounded-lg scrollbar-w-[4px]">
-          <ul className="flex min-h-full flex-col justify-end gap-2 overflow-y-auto">
-            {messages.map((message, i) => (
-              <Message
-                key={i}
-                text={message?.text ?? '[Broken message]'}
-                fromCurrentUser={chat.userId === message?.senderId}
-              />
-            ))}
-          </ul>
+          <MessagesList messages={messages} userId={chat.userId} companionId={chat.companionId} />
           <span ref={bottomOfListRef} />
         </div>
       ) : (
