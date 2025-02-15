c06f0984aef4308b50caf2844d7ec831369907f7
Anton Kachan <aa.kvchvn@gmail.com>
2025-02-15 19:42:00 +0300

    feat(message): keep lines breaks

--- app/src/components/chat/message.tsx
+++ app/src/components/chat/message.tsx
@@ -9,7 +9,7 @@ export const Message = ({ text, fromCurrentUser }: Props) => {
   return (
     <li
       className={cn(
-        'w-fit max-w-[80%] break-words rounded-3xl border px-6 py-1 leading-6',
+        'w-fit max-w-[80%] whitespace-pre-line break-words rounded-3xl border px-6 py-1 leading-6',
         fromCurrentUser &&
           'self-message self-end border-primary-hover-light bg-primary-light dark:border-primary-hover-dark dark:bg-primary-dark',
         !fromCurrentUser && 'companion-message border-zinc-400'
