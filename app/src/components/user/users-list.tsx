9b2d1aa5d436c95e9ffecb50fca2b817a096eac4
Anton Kachan <aa.kvchvn@gmail.com>
2025-02-17 22:24:07 +0300

    chore(users-list): fetch users on the client

--- app/src/components/user/users-list.tsx
+++ app/src/components/user/users-list.tsx
@@ -1,8 +1,42 @@
-import { UserItemMemo } from '~/components/user/user-item';
-import { api } from '~/trpc/server';
+'use client';
 
-export const UsersList = async () => {
-  const allUsers = await api.users.getAll();
+import { RefreshCw } from 'lucide-react';
+import { LoadingIcon } from '~/components/ui/loading-icon';
+import { UserItemMemo } from '~/components/user/user-item';
+import { api } from '~/trpc/react';
+import { Button } from '../ui/button';
+
+export const UsersList = () => {
+  const {
+    isLoading,
+    isError,
+    data: allUsers,
+    refetch,
+  } = api.users.getAll.useQuery(undefined, {
+    retry: false,
+  });
+
+  const handleClick = async () => {
+    await refetch();
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <LoadingIcon className="h-8 w-8" />
+      </div>
+    );
+  }
+
+  if (isError || !allUsers) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Button onClick={handleClick} size="icon" className="rounded-full">
+          <RefreshCw />
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <ul className="flex flex-col">
