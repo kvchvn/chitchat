c788f58704eb1543cee56e1a34f6b23329eb5792
Anton Kachan <aa.kvchvn@gmail.com>
2025-02-22 01:54:19 +0300

    chore(user-item): improve imagination

--- app/src/components/user/user-item.tsx
+++ app/src/components/user/user-item.tsx
@@ -34,7 +34,7 @@ export const UserItem = ({
       <Link
         href={`/${id}`}
         className={clsx(
-          'flex min-w-max items-center gap-2 px-2 py-4 hover:bg-primary-hover-light active:bg-primary-active-light dark:border-b-slate-800 dark:hover:bg-primary-hover-dark dark:active:bg-primary-active-dark',
+          'flex items-center gap-2 px-2 py-4 hover:bg-primary-hover-light active:bg-primary-active-light dark:border-b-slate-800 dark:hover:bg-primary-hover-dark dark:active:bg-primary-active-dark',
           {
             'bg-primary-hover-light dark:bg-primary-hover-dark': idFromPathname === id,
           }
@@ -47,23 +47,26 @@ export const UserItem = ({
           />
           <AvatarFallback className="text-sm">{getNameInitials(name)}</AvatarFallback>
         </Avatar>
-        <div className="flex min-w-32 flex-col">
-          <span className="text-sm font-semibold">{isCurrentUser ? 'Notes' : name}</span>
-          {lastMessage ? (
-            <div className="flex gap-1 text-xs">
-              <span className="font-mono text-gray-400">{lastMessageTime}</span>
-              <span>|</span>
-              <span className="line-clamp-1 max-w-40 text-ellipsis">{lastMessage.text}</span>
-            </div>
-          ) : (
-            <span className="text-xs italic text-gray-400">No messages</span>
-          )}
+        <div className="flex w-full min-w-32 flex-col">
+          <div className="flex items-center justify-between gap-2">
+            <span className="shrink-0 text-sm font-semibold">{isCurrentUser ? 'Notes' : name}</span>
+            <span className="font-mono text-xs text-gray-400">{lastMessageTime}</span>
+          </div>
+          <div className="flex items-center justify-between gap-2">
+            {lastMessage ? (
+              <span className="line-clamp-1 min-w-[50%] max-w-[70%] text-ellipsis break-words text-sm">
+                {lastMessage.text}
+              </span>
+            ) : (
+              <span className="text-sm italic text-gray-400">No messages</span>
+            )}
+            {unreadMessagesCount ? (
+              <span className="flex h-5 w-5 items-center justify-center rounded-full bg-highlight-light font-mono text-xs leading-none dark:bg-highlight-dark">
+                {unreadMessagesCount > 9 ? '9+' : unreadMessagesCount}
+              </span>
+            ) : null}
+          </div>
         </div>
-        {unreadMessagesCount ? (
-          <span className="ml-auto flex h-6 w-6 items-center justify-center rounded-full bg-highlight-light font-mono text-xs leading-none dark:bg-highlight-dark">
-            {unreadMessagesCount > 9 ? '9+' : unreadMessagesCount}
-          </span>
-        ) : null}
       </Link>
     </li>
   );
