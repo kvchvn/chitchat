28ef3e012271522ec2049dc26dcdaee15c18f94c
Anton Kachan <aa.kvchvn@gmail.com>
2025-06-03 16:56:43 +0300

    fix(env): fix runtime errors of built app

--- app/src/env.js
+++ app/src/env.js
@@ -7,16 +7,10 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string(),
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
     NEXTAUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string() : z.string().optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
-    ),
+    NEXTAUTH_URL: z.string(),
     GITHUB_ID: z.string(),
     GITHUB_SECRET: z.string(),
 
