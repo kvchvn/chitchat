93809160e4e7f603f28a287e4f54c224772f0a32
Anton Kachan <aa.kvchvn@gmail.com>
2025-06-06 17:56:34 +0300

    chore(app): improve next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,5 @@
 import { withSentryConfig } from '@sentry/nextjs';
+import { PHASE_DEVELOPMENT_SERVER } from 'next/dist/shared/lib/constants.js';
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -6,50 +7,56 @@ import { withSentryConfig } from '@sentry/nextjs';
 await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ['pino', 'pino-pretty'],
-  },
-  images: {
-    remotePatterns: [
-      {
-        // github
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        // google
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        // yandex
-        protocol: 'https',
-        hostname: 'avatars.yandex.net',
-      },
-      {
-        // facebook
-        protocol: 'https',
-        hostname: 'platform-lookaside.fbsbx.com',
-      },
-      {
-        // uploadthing
-        protocol: 'https',
-        hostname: 'cgaeud8ce2.ufs.sh',
-      },
-    ],
-  },
+const config = (phase) => {
+  const nextConfig = {
+    // Required for build via Dockerfile.standalone
+    output: phase === PHASE_DEVELOPMENT_SERVER ? undefined : 'standalone',
+    reactStrictMode: true,
+    experimental: {
+      serverComponentsExternalPackages: ['pino', 'pino-pretty'],
+    },
+    images: {
+      remotePatterns: [
+        {
+          // github
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+        },
+        {
+          // google
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+        },
+        {
+          // yandex
+          protocol: 'https',
+          hostname: 'avatars.yandex.net',
+        },
+        {
+          // facebook
+          protocol: 'https',
+          hostname: 'platform-lookaside.fbsbx.com',
+        },
+        {
+          // uploadthing
+          protocol: 'https',
+          hostname: 'cgaeud8ce2.ufs.sh',
+        },
+      ],
+    },
+  };
+
+  return withSentryConfig(nextConfig, {
+    org: 'anton-kachan',
+    project: 'chitchat-v2',
+    disableLogger: true,
+    telemetry: false,
+    widenClientFileUpload: true,
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+  });
 };
 
-export default withSentryConfig(config, {
-  org: 'anton-kachan',
-  project: 'chitchat-v2',
-  disableLogger: true,
-  telemetry: false,
-  widenClientFileUpload: true,
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-});
+export default config;
